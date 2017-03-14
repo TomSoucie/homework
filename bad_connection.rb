82c67aea502ff84dd7322a7e645948660ccc13a9
Thomas Soucie <tsoucie1858@gmail.com>
2017-03-14 09:28:29 -0600

    Removes non-homework lines

--- bad_connection.rb
+++ bad_connection.rb
@@ -8,7 +8,3 @@
     #if the input is made in all upper-case letters output "NO, THIS IS NOT A PET STORE" to the screen
     #after the first time the user input is "GOODBYE!" output "ANYTHING ELSE I CAN HELP WITH?" to the screen
     #finally after the second time of user input "GOODBYE!" output "THANK YOU FOR CALLING!" to the screen and exit the program
-
-#output
-
-#new line
