36a6a242ea72865828f82249eebc41ada11c271f
Thomas Soucie <tsoucie1858@gmail.com>
2017-03-14 09:08:53 -0600

    Initial commit 03/14/2017

---
+++ bad_connection.rb
@@ -0,0 +1,11 @@
+# Pseudocode to outline the Bad Connection program
+
+#get input from the user
+
+#based on the user input do some action
+    #if the input is "blank" (simply hit 'enter') output "HELLO?!" to the screen
+    #if the input contains any lower case letters output "I AM HAVING A HARD TIME HEARING YOU." to the screen
+    #if the input is made in all upper-case letters output "NO, THIS IS NOT A PET STORE" to the screen
+    #after the first time the user input is "GOODBYE!" output "ANYTHING ELSE I CAN HELP WITH?" to the screen
+    #finally after the second time of user input "GOODBYE!" output "THANK YOU FOR CALLING!" to the screen and exit the program
+
